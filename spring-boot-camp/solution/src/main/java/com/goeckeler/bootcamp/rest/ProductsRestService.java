c4f85262dee94eb5685cb95d9a35ea42ad63d8e6
goeckeler <thorsten@goeckeler.com>
2015-03-18 08:40:59 +0100

    Use static import for RequestMethod.

--- spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/rest/ProductsRestService.java
+++ spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/rest/ProductsRestService.java
@@ -1,8 +1,9 @@
 package com.goeckeler.bootcamp.rest;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,12 @@ import com.goeckeler.bootcamp.domain.products.object.Products;
 @RequestMapping(value = "/products")
 public class ProductsRestService
 {
-  @RequestMapping(method = RequestMethod.GET, value = "/search")
+  @RequestMapping(method = GET, value = "/search")
   public Products search(@RequestParam(value = "artist-name", required = false) String artistName) {
     return new Products();
   }
   
-  @RequestMapping(method = RequestMethod.GET, value="/{id}")
+  @RequestMapping(method = GET, value="/{id}")
   public Product findById(@PathVariable Long id) {
     return new Product();
   }
