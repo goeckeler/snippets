7977a5a6e7f87a8165a8b03137b82a9181392381
goeckeler <thorsten@goeckeler.com>
2015-03-18 08:04:35 +0100

    Moved Products and Product to commons. Added findById() to REST service to demonstrate PathVariable.

--- spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/rest/ProductsRestService.java
+++ spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/rest/ProductsRestService.java
@@ -1,7 +1,6 @@
 package com.goeckeler.bootcamp.rest;
 
-import javax.websocket.server.PathParam;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +19,7 @@ public class ProductsRestService
   }
   
   @RequestMapping(method = RequestMethod.GET, value="/{id}")
-  public Product findById(@PathParam("id") Long identifier) {
+  public Product findById(@PathVariable Long id) {
     return new Product();
   }
 }
