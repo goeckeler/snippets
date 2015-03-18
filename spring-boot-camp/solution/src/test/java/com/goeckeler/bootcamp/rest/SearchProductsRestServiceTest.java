9c9b067fb9c01a8ff2b3b502d2ef396d5dc58739
goeckeler <thorsten@goeckeler.com>
2015-03-18 22:26:03 +0100

    Added integration and system test.

---
+++ spring-boot-camp/solution/src/test/java/com/goeckeler/bootcamp/rest/SearchProductsRestServiceTest.java
@@ -0,0 +1,52 @@
+package com.goeckeler.bootcamp.rest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.annotation.IfProfileValue;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+@IfProfileValue(name = "junit.stage", values = {
+  "system", "integration"
+})
+public class SearchProductsRestServiceTest
+{
+  private MockMvc mvc;
+
+  @Before
+  public void setUp()
+    throws Exception
+  {
+    mvc = MockMvcBuilders.standaloneSetup(new ProductsRestService()).build();
+  }
+
+  @Test
+  public void shouldReturnNoResults()
+    throws Exception
+  {
+    mvc.perform(MockMvcRequestBuilders.get("/products/search?artist-name=celine").accept(MediaType.APPLICATION_JSON)).andExpect(
+        status().isOk()).andExpect(content().string(equalTo("{}")));
+  }
+
+  @Test
+  public void shouldReturnOneProduct()
+    throws Exception
+  {
+    mvc.perform(MockMvcRequestBuilders.get("/products/search?artist-name=pink").accept(MediaType.APPLICATION_JSON)).andExpect(
+        status().isOk()).andExpect(content().string(equalTo("{name=Party}")));
+  }
+}
