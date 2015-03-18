a58e95ccf93c040792cc3eb7eeff08d362195ca5
goeckeler <thorsten@goeckeler.com>
2015-03-18 07:30:23 +0100

    Switched to @SpringBootApplication.

--- spring-boot-camp/v2/src/main/java/com/goeckeler/bootcamp/Application.java
+++ spring-boot-camp/v2/src/main/java/com/goeckeler/bootcamp/Application.java
@@ -2,10 +2,10 @@ package com.goeckeler.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
