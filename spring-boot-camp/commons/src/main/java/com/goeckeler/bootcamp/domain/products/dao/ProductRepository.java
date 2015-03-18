38da5c9c641f5afca78803654d90eabc1bae334b
goeckeler <thorsten@goeckeler.com>
2015-03-18 22:25:06 +0100

    Added products repository.

---
+++ spring-boot-camp/commons/src/main/java/com/goeckeler/bootcamp/domain/products/dao/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.goeckeler.bootcamp.domain.products.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.goeckeler.bootcamp.domain.products.object.Product;
+
+/**
+ * DAO for entity {@link Product}.
+ */
+public interface ProductRepository extends CrudRepository<Product, Long>
+{
+}
