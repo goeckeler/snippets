e13fbc683d5780e1b3a2aabc20e83cf04f91f052
goeckeler <thorsten@goeckeler.com>
2015-03-19 03:45:50 +0100

    Added artists to products.

--- spring-boot-camp/commons/src/main/java/com/goeckeler/bootcamp/domain/products/object/Product.java
+++ spring-boot-camp/commons/src/main/java/com/goeckeler/bootcamp/domain/products/object/Product.java
@@ -2,16 +2,22 @@ package com.goeckeler.bootcamp.domain.products.object;
 
 import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
 
+import java.util.Arrays;
+
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="products")
-public class Product  implements Comparable<Product>
+import com.goeckeler.bootcamp.domain.artists.object.Artist;
+import com.goeckeler.bootcamp.domain.artists.object.Artists;
 
+@Entity
+@Table(name = "products")
+public class Product
+  implements Comparable<Product>
 {
   @Id
   @GeneratedValue
@@ -20,26 +26,41 @@ public class Product  implements Comparable<Product>
   @Column(length = 80)
   private String name;
 
+  @Embedded
+  private Artists artists;
+
   public Product() {
     this(null);
   }
 
   public Product(String name) {
+    this(name, (Artist[]) null);
+  }
+
+  public Product(String name, Artist... artists) {
     this.name = name;
+    this.artists = new Artists();
+    if (artists != null) {
+      Arrays.asList(artists).forEach(a -> this.artists.add(a));
+    }
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public String getName() {
     return defaultIfBlank(name, "(n/a)");
   }
 
-  public Long getId() {
-    return id;
-  }
-  
   public void setName(final String name) {
     this.name = name;
   }
 
+  public Artists getArtists() {
+    return artists;
+  }
+  
   @Override
   public int hashCode() {
     return toString().hashCode();
@@ -65,6 +86,6 @@ public class Product  implements Comparable<Product>
 
   @Override
   public String toString() {
-    return getName();
+    return getName() + " by " + getArtists().toString();
   }
 }
