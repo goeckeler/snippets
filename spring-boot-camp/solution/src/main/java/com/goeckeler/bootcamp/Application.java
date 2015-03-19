b71b0b7a75a4552b73a6b9034a2dcd13581c5705
goeckeler <thorsten@goeckeler.com>
2015-03-19 03:47:27 +0100

    Add default data at startup. To be augmented by using a profile :-).

--- spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/Application.java
+++ spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/Application.java
@@ -1,11 +1,25 @@
 package com.goeckeler.bootcamp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goeckeler.bootcamp.service.boot.Catalog;
 
 @SpringBootApplication
-public class Application {
-	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application
+{
+  @Bean
+  @Transactional
+  CommandLineRunner init(final Catalog catalog) {
+    return (event) -> {
+      catalog.load();
+    };
+  }
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
