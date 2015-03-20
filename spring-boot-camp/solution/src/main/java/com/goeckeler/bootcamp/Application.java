219d68534e3637c4525d85f623fa41779932f9fe
goeckeler <thorsten@goeckeler.com>
2015-03-20 02:56:47 +0100

    Enable aspects on application start.

--- spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/Application.java
+++ spring-boot-camp/solution/src/main/java/com/goeckeler/bootcamp/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.goeckeler.bootcamp.service.boot.Catalog;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Application
 {
   @Value("${bootcamp.sampling}")
